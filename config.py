1c5a4a27da73e0a3b3fd09be1d19dd94b5d03387
Roman Klát <jimrkh@gmail.com>
2017-04-08 17:04:00 +0200

    Influxdb requirements

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # InfluxDB
-DB_HOST=''
+INFLUXDB_HOST = 'localhost'
 
 
 try:
     from config_local import *
 except ImportError:
     raise ValueError("Please, provide local configuration file named 'config_local.py'.")
-
