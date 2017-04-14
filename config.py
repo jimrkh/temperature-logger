1c97de231fa3a670e532b4d2b16e5e159026b0c0
Roman Klat <jimrkh@gmail.com>
2017-04-14 08:12:01 +0200

    Influxdb in config file

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # InfluxDB
-INFLUXDB_HOST = 'localhost'
+INFLUXDB_HOST = 'localhost' 
+INFLUXDB_PORT = 8086
+INFLUXDB_DB = 'home'
+INFLUXDB_NAME = ''
+INFLUXDB_PASSWD = ''
 
 
 try:
     from config_local import *
 except ImportError:
     raise ValueError("Please, provide local configuration file named 'config_local.py'.")
+
--- temperature-logger.py
+++ temperature-logger.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import time
 import schedule
 
 from w1thermsensor import W1ThermSensor
 from config import (
-    INFLUXDB_HOST
+    INFLUXDB_HOST, INFLUXDB_PORT, INFLUXDB_DB, INFLUXDB_NAME, INFLUXDB_PASSWD
 )
 
 
 def job():
     for sensor in W1ThermSensor.get_available_sensors():
-        print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+        print("Sensor %s has temperature %.2f at %s" % (sensor.id, sensor.get_temperature(), time.time()))
 
 
 def main():
     schedule.every(1).minutes.do(job)
     while True:
         schedule.run_pending()
+        time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
+
